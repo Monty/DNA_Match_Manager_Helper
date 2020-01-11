d9a2906511f3bd7c9df0e8ff6006ac4f01e86f1d
Monty Williams <monty.williams@gmail.com>
2020-01-10 19:57:42 -0800

    Revise filename selection

--- makeAllDNASPreadsheets.sh
+++ makeAllDNASPreadsheets.sh
@@ -4,12 +4,12 @@
 DATE="$(date +%y%m%d)"
 LONGDATE="$(date +%y%m%d.%H%M%S)"
 
-AN_CURRENT=$(ls -1t MatchManagerExport-Ancestry_*.csv | head -1)
-FT_CURRENT=$(ls -1t MatchManagerExport-FTDNA_*.csv | head -1)
-GM_CURRENT=$(ls -1t MatchManagerExport-GEDmatch_*.csv | head -1)
-MH_CURRENT=$(ls -1t MatchManagerExport-MyHeritage_*.csv | head -1)
-TW_CURRENT=$(ls -1t MatchManagerExport-23andMe_*.csv | head -1)
-ALL_CURRENT=$(ls -1t all-*csv | head -1)
+AN_CURRENT=$(ls -1t *Ancestry_*.csv | head -1)
+FT_CURRENT=$(ls -1t *FTDNA_*.csv | head -1)
+GM_CURRENT=$(ls -1t *GEDmatch_*.csv | head -1)
+MH_CURRENT=$(ls -1t *MyHeritage_*.csv | head -1)
+TW_CURRENT=$(ls -1t *23andMe_*.csv | head -1)
+RELATIVES_CURRENT=$(ls -1t Relatives-*csv | head -1)
 
 echo "Processing these current files:"
 echo $AN_CURRENT
@@ -17,7 +17,8 @@ echo $FT_CURRENT
 echo $GM_CURRENT
 echo $MH_CURRENT
 echo $TW_CURRENT
-echo $ALL_CURRENT
+echo $RELATIVES_CURRENT
+echo ""
 
 AN_NEW="AN-$DATE.csv"
 FT_NEW="FT-$DATE.csv"
@@ -25,7 +26,11 @@ GM_NEW="GM-$DATE.csv"
 MH_NEW="MH-$DATE.csv"
 TW_NEW="TW-$DATE.csv"
 
-ALL_NEW="all-$LONGDATE.csv"
+RELATIVES_NEW="Relatives-$LONGDATE.csv"
+
+echo "Producing this new file:"
+echo "$RELATIVES_NEW"
+echo ""
 
 ./makeOneDNASPreadsheet.sh $AN_CURRENT >$AN_NEW
 ./makeOneDNASPreadsheet.sh $FT_CURRENT >$FT_NEW
@@ -33,13 +38,13 @@ ALL_NEW="all-$LONGDATE.csv"
 ./makeOneDNASPreadsheet.sh $MH_CURRENT >$MH_NEW
 ./makeOneDNASPreadsheet.sh $TW_CURRENT >$TW_NEW
 
-cp head.txt $ALL_NEW
+cp head.txt $RELATIVES_NEW
 grep -vh "^Source Site" $AN_NEW $FT_NEW $GM_NEW $MH_NEW $TW_NEW | sort -u |
-    sort --field-separator=$'\t' --key=6,6nr --key=5,5nr -f --key=2,2 >>$ALL_NEW
+    sort --field-separator=$'\t' --key=6,6nr --key=5,5nr -f --key=2,2 >>$RELATIVES_NEW
 
 echo "Differences from previous run:"
-diff -U 1 $ALL_CURRENT $ALL_NEW
+diff -U 1 $RELATIVES_CURRENT $RELATIVES_NEW
 
 echo ""
 echo "Additions from previous run:"
-diff $ALL_CURRENT $ALL_NEW | grep "^>"
+diff $RELATIVES_CURRENT $RELATIVES_NEW | grep "^>"
