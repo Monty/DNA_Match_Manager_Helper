18768d477d02455ff8589df168a824a406d99e85
Monty Williams <monty.williams@gmail.com>
2020-01-12 17:01:31 -0800

    Refactor to reduce number of files
    No need to keep individual files corresponding to each processed file. Just keep the final sorted spreadsheet. It's simple to separate them back out using grep.

--- makeAllDNASPreadsheets.sh
+++ makeAllDNASPreadsheets.sh
@@ -4,46 +4,46 @@
 DATE="$(date +%y%m%d)"
 LONGDATE="$(date +%y%m%d.%H%M%S)"
 
-AN_CURRENT=$(ls -1t *Ancestry_*.csv | head -1)
-FT_CURRENT=$(ls -1t *FTDNA_*.csv | head -1)
-GM_CURRENT=$(ls -1t *GEDmatch_*.csv | head -1)
-MH_CURRENT=$(ls -1t *MyHeritage_*.csv | head -1)
-TW_CURRENT=$(ls -1t *23andMe_*.csv | head -1)
-RELATIVES_CURRENT=$(ls -1t Relatives-*csv | head -1)
-
-echo "Processing these current files:"
-echo $AN_CURRENT
-echo $FT_CURRENT
-echo $GM_CURRENT
-echo $MH_CURRENT
-echo $TW_CURRENT
-echo $RELATIVES_CURRENT
-echo ""
-
-AN_NEW="AN-$DATE.csv"
-FT_NEW="FT-$DATE.csv"
-GM_NEW="GM-$DATE.csv"
-MH_NEW="MH-$DATE.csv"
-TW_NEW="TW-$DATE.csv"
-
+# Generated spreadsheets
 RELATIVES_NEW="Relatives-$LONGDATE.csv"
+RELATIVES_TMP="Relatives-$LONGDATE.tmp"
+touch $RELATIVES_TMP
+# Latest previously generated spreadsheet
+find . -name "Relatives*.csv" -maxdepth 1 | grep -q '^.'
+if [ $? == 0 ]; then
+    RELATIVES_CURRENT=$(ls -1t Relatives-*csv | head -1)
+fi
 
-echo "Producing this new file:"
-echo "$RELATIVES_NEW"
+# Process files downloaded by DNA Match Manager
+# Use the latest .csv files with names containing one of these strings
+KEYS=(23andMe Ancestry FTDNA GEDmatch MyHeritage)
+#
+echo "==> Processing these current files:"
+for i in "${KEYS[@]}"; do
+    target="*$i*.csv"
+    find . -name "$target" -maxdepth 1 | grep -q '^.'
+    if [ $? == 0 ]; then
+        CURRENT_FILE=$(ls -1t $target | head -1)
+        echo $CURRENT_FILE
+        ./makeOneDNASPreadsheet.sh $CURRENT_FILE >>$RELATIVES_TMP
+    fi
+done
 echo ""
 
-./makeOneDNASPreadsheet.sh $AN_CURRENT >$AN_NEW
-./makeOneDNASPreadsheet.sh $FT_CURRENT >$FT_NEW
-./makeOneDNASPreadsheet.sh $GM_CURRENT >$GM_NEW
-./makeOneDNASPreadsheet.sh $MH_CURRENT >$MH_NEW
-./makeOneDNASPreadsheet.sh $TW_CURRENT >$TW_NEW
-
-sort -u $AN_NEW $FT_NEW $GM_NEW $MH_NEW $TW_NEW |
+echo "==> Producing this new spreadsheet:"
+echo "$RELATIVES_NEW"
+# Sort by cmS, pct, and then name
+sort -u $RELATIVES_TMP |
     sort --field-separator=$'\t' --key=6,6nr --key=5,5nr -f --key=2,2 >$RELATIVES_NEW
+rm -f $RELATIVES_TMP
 
-echo "Differences from previous run:"
+# If there is no current spreadsheet to compare iagainst then exit
+[ -z "$RELATIVES_CURRENT" ] && exit
+
+echo ""
+echo "==> Differences from previous run:"
 diff -U 1 $RELATIVES_CURRENT $RELATIVES_NEW
 
 echo ""
-echo "Additions from previous run:"
+echo "==> Additions from previous run:"
 diff $RELATIVES_CURRENT $RELATIVES_NEW | grep "^>"
